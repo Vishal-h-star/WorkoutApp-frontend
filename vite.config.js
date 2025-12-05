0f5c363e7b57ecf41b439a670af7adcb9711252c
vishal-h-star <vc8368@gmail.com>
2025-12-05 16:53:31 +0530

    final updated

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
-      //  "/api": "http://localhost:4000",
-    //  "/api": "https://wokoutapp-backend.onrender.com",
-    }
+    // proxy:{
+    //   //  "/api": "http://localhost:4000",
+    // //  "/api": "https://wokoutapp-backend.onrender.com",
+    // }
   }
 })
